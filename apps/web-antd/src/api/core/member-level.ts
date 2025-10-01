a2b155efb25e13e702031eda8b4c2240ca75f6a9
ayou <guoxinlee129@gmail.com>
2025-10-02 01:52:47 +0800

    fix: 资源功能

--- apps/web-antd/src/api/core/member-level.ts
+++ apps/web-antd/src/api/core/member-level.ts
@@ -15,7 +15,7 @@ import { apiPrefix } from './config';
  */
 export async function getMemberLevelPageApi(data: RequestGetPageQuery) {
   return requestClient.post<PhpPageResponse<MemberLevelVO>>(
-    `${apiPrefix}/user/member-levels/list/page`,
+    `${apiPrefix}/user-member-level/list/page`,
     data,
   );
 }
@@ -24,14 +24,14 @@ export async function getMemberLevelPageApi(data: RequestGetPageQuery) {
  * 创建会员等级
  */
 export async function createMemberLevelApi(data: MemberLevelDTO) {
-  return requestClient.post(`${apiPrefix}/user/member-levels`, data);
+  return requestClient.post(`${apiPrefix}/user-member-level`, data);
 }
 
 /**
  * 更新会员等级
  */
 export async function updateMemberLevelApi(id: number, data: MemberLevelDTO) {
-  return requestClient.put(`${apiPrefix}/user/member-levels/${id}`, data);
+  return requestClient.put(`${apiPrefix}/user-member-level/${id}`, data);
 }
 
 /**
@@ -41,15 +41,12 @@ export async function changeMemberLevelStatusApi(
   id: number,
   data: MemberLevelStatusDTO,
 ) {
-  return requestClient.put(
-    `${apiPrefix}/user/member-levels/${id}/status`,
-    data,
-  );
+  return requestClient.put(`${apiPrefix}/user-member-level/${id}/status`, data);
 }
 
 /**
  * 删除会员等级
  */
 export async function deleteMemberLevelApi(id: number) {
-  return requestClient.delete(`${apiPrefix}/user/member-levels/${id}`);
+  return requestClient.delete(`${apiPrefix}/user-member-level/${id}`);
 }
